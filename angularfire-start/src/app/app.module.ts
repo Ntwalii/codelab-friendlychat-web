8057d16a1499f0df13a6215357702abfd005e51f
Ntwalii <ntwaliaubin@gmail.com>
2023-10-19 18:35:47 +0300

    Tried debugging

--- angularfire-start/src/app/app.module.ts
+++ angularfire-start/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
--- angularfire-start/src/app/services/chat.service.ts
+++ angularfire-start/src/app/services/chat.service.ts
@@ -1,4 +1,5 @@
 import { inject, Injectable } from '@angular/core';
+
 import {
   Auth,
   authState,
@@ -9,7 +10,7 @@ import {
   getAuth,
   User,
 } from '@angular/fire/auth';
-import { map, switchMap, firstValueFrom, filter, Observable } from 'rxjs';
+import { map, switchMap, firstValueFrom, filter, Observable, } from 'rxjs';
 import {
   doc,
   docData,
@@ -55,16 +56,56 @@ export class ChatService {
   user$ = user(this.auth);
 
   // Login Friendly Chat.
-  login() {}
-
-  // Logout of Friendly Chat.
-  logout() {}
-
+// Signs-in Friendly Chat.
+login() {
+  signInWithPopup(this.auth, this.provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      this.router.navigate(['/', 'chat']);
+      return credential;
+  })
+}
+// Logout of Friendly Chat.
+logout() {
+  signOut(this.auth).then(() => {
+      this.router.navigate(['/', 'login'])
+      console.log('signed out');
+  }).catch((error) => {
+      console.log('sign out error: ' + error);
+  })
+}
   // Adds a text or image message to Cloud Firestore.
-  addMessage = async (
-    textMessage: string | null,
-    imageUrl: string | null
-  ): Promise<void | DocumentReference<DocumentData>> => {};
+ // Adds a text or image message to Cloud Firestore.
+addMessage = async(textMessage: string | null, imageUrl: string | null): Promise<void | DocumentReference<DocumentData>> => {
+  let data: any;
+  try {
+    this.user$.subscribe(async (user) => 
+    { 
+      if(textMessage && textMessage.length > 0) {
+        data =  await addDoc(collection(this.firestore, 'messages'), {
+          name: user?.displayName,
+          text: textMessage,
+          profilePicUrl: user?.photoURL,
+          timestamp: serverTimestamp(),
+          uid: user?.uid
+        })}
+        else if (imageUrl && imageUrl.length > 0) {
+          data =  await addDoc(collection(this.firestore, 'messages'), {
+            name: user?.displayName,
+            imageUrl: imageUrl,
+            profilePicUrl: user?.photoURL,
+            timestamp: serverTimestamp(),
+            uid: user?.uid
+          });
+        }
+        return data;
+      }
+    );
+  }
+  catch(error) {
+    console.error('Error writing new message to Firebase Database', error);
+    return;
+  }
+}
 
   // Saves a new message to Cloud Firestore.
   saveTextMessage = async (messageText: string) => {
@@ -72,9 +113,13 @@ export class ChatService {
   };
 
   // Loads chat messages history and listens for upcoming ones.
-  loadMessages = () => {
-    return null as unknown;
-  };
+// Loads chat message history and listens for upcoming ones.
+loadMessages = () => {
+  // Create the query to load the last 12 messages and listen for new ones.
+  const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'desc'), limit(12));
+  // Start listening to the query.
+  return collectionData(recentMessagesQuery);
+}
 
   // Saves a new message containing an image in Firebase.
   // This first saves the image in Firebase storage.
@@ -96,7 +141,43 @@ export class ChatService {
     return null;
   }
   // Requests permissions to show notifications.
-  requestNotificationsPermissions = async () => {};
+ // Requests permissions to show notifications.
+requestNotificationsPermissions = async () => {
+  console.log('Requesting notifications permission...');
+  const permission = await Notification.requestPermission();
+  
+  if (permission === 'granted') {
+    console.log('Notification permission granted.');
+    // Notification permission granted.
+    await this.saveMessagingDeviceToken();
+  } else {
+    console.log('Unable to get permission to notify.');
+  }
+}
+ // Saves the messaging device token to Cloud Firestore.
+saveMessagingDeviceToken= async () => {
+  try {
+    const currentToken = await getToken(this.messaging);
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to Cloud Firestore.
+      const tokenRef = doc(this.firestore, 'fcmTokens', currentToken);
+      await setDoc(tokenRef, { uid: this.auth.currentUser?.uid });
 
-  saveMessagingDeviceToken = async () => {};
+      // This will fire when a message is received while the app is in the foreground.
+      // When the app is in the background, firebase-messaging-sw.js will receive the message instead.
+      onMessage(this.messaging, (message) => {
+        console.log(
+          'New foreground notification from Firebase Messaging!',
+          message.notification
+        );
+      });
+    } else {
+      // Need to request permissions to show notifications.
+      this.requestNotificationsPermissions();
+    }
+  } catch(error) {
+    console.error('Unable to get messaging token.', error);
+  };
+}
 }
